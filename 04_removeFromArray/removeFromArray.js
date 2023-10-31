42f2498053c9ffb389bb57505a2d45da5e88fa55
Charminha <83584290+Charminha@users.noreply.github.com>
2023-10-31 20:24:58 +0100

    Add comments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,9 @@
 const removeFromArray = function(array) {
-    
+    // Looping through arguments object
     for (let i = 0; i < arguments.length; i++){
+        // each argument is compared with each array element
         for (let j = 0; j < array.length; j++){
+            // identical elements are removed using splice()
             if (array[j] === arguments[i]){
                 array.splice(j, 1);
             }
