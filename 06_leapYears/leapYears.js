976edec3e762259480502b81e090494f694fcedc
Charminha <83584290+Charminha@users.noreply.github.com>
2023-10-31 21:05:23 +0100

    Complete exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,10 +1,14 @@
 const leapYears = function(year) {
     // check if divisible by 4
     if (year%4 == 0) {
+        // non-century years divisble by 4 are leap years
         if (year%100 != 0) return true;
+        // century years divisible by 400 are leap years
         else if (year%400 == 0) return true;
+        // century years that aren't divisible by 400 aren't leap years
         else return false;
     }
+    // years not divisible by 4 are definitely not leap years
     else return false;
 };
 
