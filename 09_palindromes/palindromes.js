40bd991b79eea8bcb2750d29d21e8fb9c2bf2d2b
Charminha <83584290+Charminha@users.noreply.github.com>
2023-12-30 15:21:14 +0100

    Solve palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,22 @@
-const palindromes = function () {
+const palindromes = function (words) {
+    // let words = "A carb, a man, a maraca?!"
+    // convert string to lower case
+    words = words.toLowerCase();
+    // remove anything that is not a letter or digit
+    words = words.replace(/[^\w\']|_/g, "");
+    let charArray = words.split('');   
+
+    // console.log(charArray);
+    let isPalindrome = true;
+    for(let i = 0; i<(charArray.length/2)-1 ; i++){
+        if(charArray[i] !== charArray[charArray.length-i-1]){
+            isPalindrome = false;
+            // console.log("set to false");
+        }
+        // console.table(charArray[i] === charArray[charArray.length-i-1], i);  
+    }
+    // console.log(isPalindrome);
+    return isPalindrome;
 
 };
 
