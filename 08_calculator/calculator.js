622dce8fdc37621e86b9e8291b2eae92735a65e2
Charminha <83584290+Charminha@users.noreply.github.com>
2023-12-30 14:02:19 +0100

    Solve exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b; 
 };
 
-const sum = function() {
-	
+let array = [];
+const sum = function(array) {
+  return array.reduce((total, a) => (total + a), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, a) => (total * a), 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  let res = base;
+  for(let i = 0; i<exponent-1; i++){
+    res *= base;
+  }
+
+  return res;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let res = 1;
+  for (let i=a+1; i>1; i--){
+    res *= i-1;
+  }
+  return res;
 };
 
 // Do not edit below this line
