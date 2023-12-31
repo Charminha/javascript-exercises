baa297d364b5fbaf74113e7f4cd78da7b50edf72
Charminha <83584290+Charminha@users.noreply.github.com>
2023-12-31 19:20:43 +0100

    Complete findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((currentOldest, currentPerson) => {
+        const currentOldestAge = getAge(currentOldest.yearOfBirth, currentOldest.yearOfDeath);
+        const currentPersonAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);  
+        
+        return currentPersonAge > currentOldestAge ? currentPerson : currentOldest;        
+    });
 };
+
+// calculate age with Date() if there is no death date
+const getAge = function (birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  };  
 
 // Do not edit below this line
 module.exports = findTheOldest;
