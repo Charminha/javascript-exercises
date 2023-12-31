81910f7108d72696f70eba6486d8386cc9e111e0
Charminha <83584290+Charminha@users.noreply.github.com>
2023-12-31 18:15:38 +0100

    Complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    // if a is a string convert to number
+    if (typeof a === "number"){
+        a = Number(a);
+    }
+    // doesn't accept negatives
+    if (a < 0) {
+        return "OOPS";
+    }
+
+    // return element at index (starting from 1) a from fibonacci series 1, 1, 2, 3
+    let fiboArray = [1, 1];
+    // add as many fibonacci number to the array as we need for the given index
+    // loop starts at 2, so array isn't expanded if asked for index 1 or 2
+    for (let i = 2; i<a; i++){
+       newNumber = fiboArray[fiboArray.length-1] + fiboArray[fiboArray.length-2];
+       fiboArray.push(newNumber);
+    }
+    return fiboArray[a-1];
+
+    //does
 
 };
 
